860fbcf94a38681f4627bdc37170d96894c758d9
zuolb3 <zuolb3@chinaunicom.cn>
2020-12-24 15:08:55 +0800

    Initial Commit

---
+++ Shared/WoDecryptApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoDecryptApp.swift
+//  Shared
+//
+//  Created by zuo on 2020/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WoDecryptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
